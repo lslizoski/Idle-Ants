7fedc00e6e986ef7ea3ac82cb8dde3180f7ab13c
DylNorr1s <dylnorrisj@gmail.com>
2023-11-04 01:30:01 -0500

    Update KivyGraphics.py

--- KivyGraphics.py
+++ KivyGraphics.py
@@ -1,1 +1,14 @@
 from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class IdleAntsApp(App):
+    pass
