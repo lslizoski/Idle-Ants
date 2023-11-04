ea149677e5c9d1cb23fd8dffca2bfdae1ced1742
DylNorr1s <dylnorrisj@gmail.com>
2023-11-04 13:08:33 -0500

    Update KivyGraphics.py
    Added float layout I think

--- KivyGraphics.py
+++ KivyGraphics.py
@@ -1,9 +1,14 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 
 
 class BoxLayouts(BoxLayout):
+    pass
+
+
+class FloatLayouts(FloatLayout):
     pass
 
 
