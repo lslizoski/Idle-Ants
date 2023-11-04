62ae90e31785ce05c6279fd98c6d05f67fbfbed4
DylNorr1s <dylnorrisj@gmail.com>
2023-11-04 08:27:24 -0500

    updated kivygraphics

--- KivyGraphics.py
+++ KivyGraphics.py
@@ -2,16 +2,10 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
-from Upgrades.Incubator import Incubator
 
 
 class BoxLayouts(BoxLayout):
-    # Method to be called when a button is pressed
-    def showSpeed(self):
-        self.incubator = Incubator()
-        self.incubator.getSpeed()
-
-
+    pass
 
 
 class MainWidget(Widget):
