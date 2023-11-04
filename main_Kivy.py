0da3f9eeb68a81fb5518c3eb456e0e555d0dfd2d
alexisbernt <alexisbernthal4@gmail.com>
2023-11-04 10:01:20 -0500

    For kivy display

--- main_Kivy.py
+++ main_Kivy.py
@@ -1,23 +1,26 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
-from Upgrades.Incubator import Incubator
+# from Upgrades.Incubator import Incubator
+from kivy.uix.image import Image
+
+# from Upgrades.Incubator import Incubator
+class BoxLayouts(BoxLayout):
+    # Method to be called when a button is pressed
+    # def showSpeed(self):
+    #     self.incubator = Incubator()
+    #     self.incubator.getSpeed()
+    pass
 
 class MainWidget(Widget):
-
     # Create Method that can be called by with the buttons
     def addInt(self):
         self.incubator = Incubator()
         self.incubator.upgradeHatchSpeed()
+
 class Line1(Widget):
-    pass
-class Line2(Widget):
-    pass
-class Line3(Widget):
-    pass
-class LineCircle(Widget):
-    pass
-class LineRectangle(Widget):
     pass
 
 class AntApp(App):
@@ -25,13 +28,21 @@ class AntApp(App):
 
 class AntApp2(App):
     def build(self):
+        layout = FloatLayout()
+        # Create an Image widget and set its source to your .png file
+        image = Image(source='anthill_tunnels.png')
+        image.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+        image.size_hint = (None, None)  # Disable size_hint so you can set a fixed size
+        image.size = (400, 400)  # Set the size of the image (adjust as needed)
+
+        # Add the image widget to the layout
+        layout.add_widget(image)
+
+        return layout
+        return image
         root = GridLayout(cols = 3, padding = 50, spacing = 100)
         root.add_widget(Line1())
-        root.add_widget(Line2())
-        root.add_widget(Line3())
-        root.add_widget(LineCircle())
-        root.add_widget(LineRectangle())
         return root
 
 AntApp().run()
-AntApp().run()
+# AntApp2().run()
