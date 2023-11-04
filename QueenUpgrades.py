e1f72a8dd57c785dc87845ff70d66fbbdc0106c3
lslizoski <lslizoski32@gmail.com>
2023-11-03 23:37:06 -0500

    Queen upgrade lay time method

--- QueenUpgrades.py
+++ QueenUpgrades.py
@@ -3,25 +3,39 @@ import os.path
 
 class QueenUpgrades:
 
-    eggLayTime = 60
-    doubleEgg = False
     queenUpgradesFile = JsonStore('QueenUpgrades.json')
+    eggLayTime = 60
+    eggMultiplier = 1
+    upgradePercentage = 0.025
+    tierOneCounter = 0
+    tierTwoCounter = 0
+    tierThreeCounter = 0
 
     def __init__(self):
         self.createFile()
-        self.setInitialVariables()
-
-    def saveUpgrades(self):
-        self.queenUpgradesFile.put('QueenUpgrades', eggLayTime=60, doubleEgg=False)
+        self.loadSavedData()
 
     def createFile(self):
-        path = './QueenUpgradess.py'
-        check_file = os.path.isfile(path)
-        print(check_file)
+        path = './QueenUpgrades.json'
+        fileExists = os.path.isfile(path)
+        if not fileExists:
+            self.queenUpgradesFile = JsonStore('QueenUpgrades.json')
+            self.setDefaultVariables()
 
-    def setInitialVariables(self):
-        #print(self.queenUpgradesFile.get('QueenUpgrades')['eggLayTime'])
-        #self.queenUpgradesFile.put('QueenUpgrades', eggLayTime=60, doubleEgg=False)
+    def loadSavedData(self):
+        self.eggLayTime = self.queenUpgradesFile.get('eggLayTime')['value']
+
+    def setDefaultVariables(self):
+        self.queenUpgradesFile.put('eggLayTime', value=self.eggLayTime, eggMultiplier=self.eggMultiplier)
+        self.queenUpgradesFile.put('eggMultiplier', value=self.eggMultiplier)
+
+    def upgradeEggLayTime(self):
+        self.eggLayTime -= self.eggLayTime * self.upgradePercentage
+        self.queenUpgradesFile.put('eggLayTime', value=self.eggLayTime)
+        print(self.eggLayTime)
+
+    def upgradeEggMultiplier(self):
         pass
+
 
 #queenUpgrades = QueenUpgrades()
--- SavingService.py
+++
@@ -1,7 +0,0 @@
-class SavingService:
-
-    def __init__(self, queenUpgrades):
-        self.queenUpgrades = queenUpgrades
-
-    def saveGame(self):
-        pass
