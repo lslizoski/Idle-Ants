d4093fe6d07f6d85e1604c32b754627d88cff72d
alexisbernt <alexisbernthal4@gmail.com>
2023-11-04 15:49:29 -0500

    Room 1

---
+++ room1.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+
+
+class BoxLayouts(BoxLayout):
+    pass
+
+
+class FloatLayouts(FloatLayout):
+    pass
+
+
+class MainWidget(Widget):
+    pass
+
+
+class Room1(App):
+    pass
+
+
+Room1().run()
