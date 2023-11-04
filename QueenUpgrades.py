932a0469e477ad7e79f45975ed295a03cc8c2cf1
lslizoski <lslizoski32@gmail.com>
2023-11-04 00:53:28 -0500

    Egg multiplier

--- QueenUpgrades.py
+++ QueenUpgrades.py
@@ -7,9 +7,11 @@ class QueenUpgrades:
     eggLayTime = 60
     eggMultiplier = 1
     upgradePercentage = 0.025
-    tierOneCounter = 0
-    tierTwoCounter = 0
-    tierThreeCounter = 0
+
+    eggLayTimeTier = 1
+    eggLayTimeTierStage = 0
+    eggMultiplierTier = 1
+    eggMultiplierTierStage = 0
 
     def __init__(self):
         self.createFile()
@@ -24,18 +26,34 @@ class QueenUpgrades:
 
     def loadSavedData(self):
         self.eggLayTime = self.queenUpgradesFile.get('eggLayTime')['value']
+        self.eggLayTimeTier = self.queenUpgradesFile.get('eggLayTime')['eggLayTimeTier']
+        self.eggLayTimeTierStage = self.queenUpgradesFile.get('eggLayTime')['eggLayTimeTierStage']
+
+        self.eggMultiplier = self.queenUpgradesFile.get('eggMultiplier')['value']
+        self.eggMultiplierTier = self.queenUpgradesFile.get('eggMultiplier')['eggMultiplierTier']
+        self.eggMultiplierTierStage = self.queenUpgradesFile.get('eggMultiplier')['eggMultiplierTierStage']
 
     def setDefaultVariables(self):
-        self.queenUpgradesFile.put('eggLayTime', value=self.eggLayTime, eggMultiplier=self.eggMultiplier)
-        self.queenUpgradesFile.put('eggMultiplier', value=self.eggMultiplier)
+        self.queenUpgradesFile.put('eggLayTime', value=self.eggLayTime, eggLayTimeTier=self.eggLayTimeTier, eggLayTimeTierStage=self.eggLayTimeTierStage)
+        self.queenUpgradesFile.put('eggMultiplier', value=self.eggMultiplier, eggMultiplierTier=self.eggMultiplierTier, eggMultiplierTierStage=self.eggMultiplierTierStage)
 
     def upgradeEggLayTime(self):
         self.eggLayTime -= self.eggLayTime * self.upgradePercentage
-        self.queenUpgradesFile.put('eggLayTime', value=self.eggLayTime)
-        print(self.eggLayTime)
+        if (self.eggLayTimeTierStage >= 5):
+            self.eggLayTimeTier += 1
+            self.eggLayTimeTierStage = 0
+        else:
+            self.eggLayTimeTierStage += 1
+
+        self.eggLayTimeTierStage += 1
+        self.queenUpgradesFile.put('eggLayTime', value=self.eggLayTime, eggLayTimeTier=self.eggLayTimeTier, eggLayTimeTierStage=self.eggLayTimeTierStage)
 
     def upgradeEggMultiplier(self):
-        pass
+        if self.eggLayTimeTier >= 2:
+            self.eggMultiplier += 1
+            self.queenUpgradesFile.put('eggMultiplier', value=self.eggMultiplier, eggMultiplierTier=self.eggMultiplierTier, eggMultiplierTierStage=self.eggMultiplierTierStage)
+        else:
+            print("Must be at least Tier II in eggLayTime")
 
 
-#queenUpgrades = QueenUpgrades()
+queenUpgrades = QueenUpgrades()
