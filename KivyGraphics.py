6bcb8124761caab676b1a4163b8b9672050c6e4e
DylNorr1s <dylnorrisj@gmail.com>
2023-11-04 09:27:39 -0500

    Revert "updated kivygraphics"
    This reverts commit 62ae90e31785ce05c6279fd98c6d05f67fbfbed4.

--- KivyGraphics.py
+++ KivyGraphics.py
@@ -2,10 +2,16 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
+from Upgrades.Incubator import Incubator
 
 
 class BoxLayouts(BoxLayout):
-    pass
+    # Method to be called when a button is pressed
+    def showSpeed(self):
+        self.incubator = Incubator()
+        self.incubator.getSpeed()
+
+
 
 
 class MainWidget(Widget):
