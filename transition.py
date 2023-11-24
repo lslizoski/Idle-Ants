5cf46238fb036488a4b48b67c68ea31b107aa193
Klayton Pagel <pagelk@lopers.unk.edu>
2023-11-24 11:04:42 -0600

    clear ants on screen when reset
    Made it so when you reset the game the ants on the screen also get removed

--- transition.py
+++ transition.py
@@ -21,6 +21,7 @@ class Home(Screen):
     queenUpgrades = QueenUpgrades(foodStorage)
     incubator = Incubator(queenUpgrades, foodStorage)
     foodGenerator = FoodGenerator()
+    antList = []
     hatchClock = 0
     layClock = 0
 
@@ -32,11 +33,13 @@ class Home(Screen):
         ant = Image(source="images/ant.png")
         ant.pos_hint = {'center_x': 0.53, 'center_y': 0.4}
         self.add_widget(ant)
-        anim = (Animation(pos_hint={'center_x': 0.77, 'center_y': 0.23}, duration=5) +
+        self.antList.append(ant)
+        self.leave_anim = (Animation(pos_hint={'center_x': 0.77, 'center_y': 0.23}, duration=5) +
                 Animation(pos_hint={'center_x': 1, 'center_y': 0.23}, duration=5))
-        anim.bind(on_complete=lambda *args : self.remove_widget(ant))
-        anim.bind(on_complete=self.ant_return_animation)
-        anim.start(ant)
+        self.leave_anim.bind(on_complete=lambda *args: self.antList.remove(ant))
+        self.leave_anim.bind(on_complete=lambda *args: self.remove_widget(ant))
+        self.leave_anim.bind(on_complete=self.ant_return_animation)
+        self.leave_anim.start(ant)
 
     def ant_return_animation(self, *args):
         food_type = randint(1, 3)
@@ -48,11 +51,13 @@ class Home(Screen):
             ant = Image(source="images/AntSugar.png")
         ant.pos_hint = {'center_x': 0, 'center_y': 0.23}
         self.add_widget(ant)
-        anim = (Animation(pos_hint={'center_x': 0.27, 'center_y': 0.23}, duration=5) +
+        self.antList.append(ant)
+        self.return_anim = (Animation(pos_hint={'center_x': 0.27, 'center_y': 0.23}, duration=5) +
                 Animation(pos_hint={'center_x': 0.47, 'center_y': 0.4}, duration=5))
-        anim.bind(on_complete=lambda *args: self.remove_widget(ant))
-        anim.bind(on_complete=lambda *args: self.foodStorage.addFood(food_type * self.foodStorage.getMultiplyTier()))
-        anim.start(ant)
+        self.return_anim.bind(on_complete=lambda *args: self.antList.remove(ant))
+        self.return_anim.bind(on_complete=lambda *args: self.remove_widget(ant))
+        self.return_anim.bind(on_complete=lambda *args: self.foodStorage.addFood(food_type * self.foodStorage.getMultiplyTier()))
+        self.return_anim.start(ant)
 
     def timers(self, interval):
         self.updateCounters()
@@ -75,12 +80,13 @@ class Home(Screen):
 
     def updateCounters(self):
         self.ids.ant_count.text = str('Ants: ' + str(self.incubator.getAnts()))
-        self.ids.food_count.text = str('Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
+        self.ids.food_count.text = str(
+            'Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
         self.ids.egg_count.text = str('Eggs: ' + str(self.queenUpgrades.getEggs()))
 
     def setAnt(self):
         self.incubator.setAnts(1)
-        if randint(0,10) == 3:
+        if randint(0, 10) == 3:
             self.foodStorage.addFood(1)
 
     def layEgg(self):
@@ -96,6 +102,22 @@ class Home(Screen):
             if self.queenUpgrades.getEggs() <= 0:
                 self.queenUpgrades.setEggs(0)
 
+    def reset_ants(self, *args):
+        for ant in self.antList:
+            self.leave_anim.cancel_all(ant)
+            self.return_anim.cancel_all(ant)
+            self.remove_widget(ant)
+        self.antList.clear()
+
+    def reset_stats(self):
+        os.remove('FoodStorage.json')
+        os.remove('Incubator.json')
+        os.remove('QueenUpgrades.json')
+        self.foodStorage.createFile()
+        self.incubator.createFile()
+        self.queenUpgrades.createFile()
+        self.reset_ants()
+
 
 class Menu(Screen):
     foodStorage = FoodStorage()
@@ -110,7 +132,8 @@ class Menu(Screen):
 
     def updateCounters(self, *args):
         self.ids.ant_count.text = str('Ants: ' + str(self.incubator.getAnts()))
-        self.ids.food_count.text = str('Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
+        self.ids.food_count.text = str(
+            'Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
         self.ids.egg_count.text = str('Eggs: ' + str(self.queenUpgrades.getEggs()))
         if self.ids.ant_progress_bar.value >= self.incubator.getHatchSpeed():
             self.ids.ant_progress_bar.value = 0
@@ -122,14 +145,6 @@ class Menu(Screen):
         self.ids.egg_progress_bar.value += 0.1
         self.ids.egg_progress_bar.max = self.queenUpgrades.getEggLaySpeed()
         self.layClock += 0.1
-
-    def reset_stats(self):
-        os.remove('FoodStorage.json')
-        os.remove('Incubator.json')
-        os.remove('QueenUpgrades.json')
-        self.foodStorage.createFile()
-        self.incubator.createFile()
-        self.queenUpgrades.createFile()
 
 
 class Queen(Screen):
@@ -151,7 +166,8 @@ class Queen(Screen):
 
     def updateCounters(self, *args):
         self.ids.ant_count.text = str('Ants: ' + str(self.incubator.getAnts()))
-        self.ids.food_count.text = str('Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
+        self.ids.food_count.text = str(
+            'Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
         self.ids.egg_count.text = str('Eggs: ' + str(self.queenUpgrades.getEggs()))
         self.ids.speed_tier.text = str('TIER:' + str(self.queenUpgrades.getLaySpeedTier()))
         self.ids.speed_stage.text = str('STAGE:' + str(self.queenUpgrades.getLaySpeedTierStage()))
@@ -175,6 +191,7 @@ class Storage(Screen):
     incubator = Incubator(queenUpgrades, foodStorage)
     hatchClock = 0
     layClock = 0
+
     def __init__(self, **kw):
         super().__init__(**kw)
         Clock.schedule_interval(self.updateCounters, 0.1)
@@ -189,7 +206,8 @@ class Storage(Screen):
 
     def updateCounters(self, *args):
         self.ids.ant_count.text = str('Ants: ' + str(self.incubator.getAnts()))
-        self.ids.food_count.text = str('Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
+        self.ids.food_count.text = str(
+            'Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
         self.ids.egg_count.text = str('Eggs: ' + str(self.queenUpgrades.getEggs()))
         self.ids.multi_tier.text = str('TIER:' + str(self.foodStorage.getMultiplyTier()))
         self.ids.multi_stage.text = str('STAGE:' + str(self.foodStorage.getMultiplyTierStage()))
@@ -224,7 +242,8 @@ class Incubator(Screen):
 
     def updateCounters(self, *args):
         self.ids.ant_count.text = str('Ants: ' + str(self.incubator.getAnts()))
-        self.ids.food_count.text = str('Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
+        self.ids.food_count.text = str(
+            'Food: ' + str(self.foodStorage.getFood()) + '/' + str(self.foodStorage.getMaxFood()))
         self.ids.egg_count.text = str('Eggs: ' + str(self.queenUpgrades.getEggs()))
         self.ids.speed_tier.text = str('TIER:' + str(self.incubator.getHatchSpeedTier()))
         self.ids.speed_stage.text = str('STAGE:' + str(self.incubator.getHatchSpeedTierStage()))
